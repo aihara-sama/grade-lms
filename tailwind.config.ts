f15f2b1b1e5266c70ea9c96af1cfdeec2c9f0460
Aihara <aihara-sama@mail.ru>
2024-07-30 19:02:29 +0300

    fix(tailwind): sugg new cmp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
 // tailwind.config.js
-import { readFileSync } from "fs";
-import postcss from "postcss";
-import postcssJs from "postcss-js";
+// import { readFileSync } from "fs";
+// import postcss from "postcss";
+// import postcssJs from "postcss-js";
 import type { Config } from "tailwindcss";
-import plugin from "tailwindcss/plugin";
+// import plugin from "tailwindcss/plugin";
 
-const globalCSS = require("./app/globals.css");
+// const globalCSS = require("./app/globals.css");
 
 // Editor sugestions for new components
-require.extensions[".css"] = function (module, filename) {
-  module.exports = plugin(({ addBase, addComponents, addUtilities }) => {
-    const css = readFileSync(filename, "utf8");
-    const root = postcss.parse(css);
-    const jss = postcssJs.objectify(root);
+// require.extensions[".css"] = function (module, filename) {
+//   module.exports = plugin(({ addBase, addComponents, addUtilities }) => {
+//     const css = readFileSync(filename, "utf8");
+//     const root = postcss.parse(css);
+//     const jss = postcssJs.objectify(root);
 
-    if ("@layer base" in jss) {
-      addBase(jss["@layer base"]);
-    }
-    if ("@layer components" in jss) {
-      addComponents(jss["@layer components"]);
-    }
-    if ("@layer utilities" in jss) {
-      addUtilities(jss["@layer utilities"]);
-    }
-  });
-};
+//     if ("@layer base" in jss) {
+//       addBase(jss["@layer base"]);
+//     }
+//     if ("@layer components" in jss) {
+//       addComponents(jss["@layer components"]);
+//     }
+//     if ("@layer utilities" in jss) {
+//       addUtilities(jss["@layer utilities"]);
+//     }
+//   });
+// };
 
 const config: Config = {
   content: [
@@ -39,6 +39,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [globalCSS],
+  // plugins: [globalCSS],
+  plugins: [],
 };
 export default config;
