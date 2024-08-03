4c9b7438c3855e5545a62546e337729994235d9d
Aihara <aihara-sama@mail.ru>
2024-08-04 00:45:57 +0300

    feat(custom-classes-sugg): intellisense for custom css classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,29 @@
 // tailwind.config.js
-// import { readFileSync } from "fs";
-// import postcss from "postcss";
-// import postcssJs from "postcss-js";
+import { readFileSync } from "fs";
+import postcss from "postcss";
+import postcssJs from "postcss-js";
 import type { Config } from "tailwindcss";
-// import plugin from "tailwindcss/plugin";
 
-// const globalCSS = require("./app/globals.css");
+import plugin from "tailwindcss/plugin";
 
-// Editor sugestions for new components
-// require.extensions[".css"] = function (module, filename) {
-//   module.exports = plugin(({ addBase, addComponents, addUtilities }) => {
-//     const css = readFileSync(filename, "utf8");
-//     const root = postcss.parse(css);
-//     const jss = postcssJs.objectify(root);
+// Editor sugestions for custom css classes
+require.extensions[".css"] = function (module, filename) {
+  module.exports = plugin(({ addBase, addComponents, addUtilities }) => {
+    const css = readFileSync(filename, "utf8");
+    const root = postcss.parse(css);
+    const jss = postcssJs.objectify(root);
 
-//     if ("@layer base" in jss) {
-//       addBase(jss["@layer base"]);
-//     }
-//     if ("@layer components" in jss) {
-//       addComponents(jss["@layer components"]);
-//     }
-//     if ("@layer utilities" in jss) {
-//       addUtilities(jss["@layer utilities"]);
-//     }
-//   });
-// };
+    if ("@layer base" in jss) {
+      addBase(jss["@layer base"]);
+    }
+    if ("@layer components" in jss) {
+      addComponents(jss["@layer components"]);
+    }
+    if ("@layer utilities" in jss) {
+      addUtilities(jss["@layer utilities"]);
+    }
+  });
+};
 
 const config: Config = {
   content: [
@@ -61,7 +60,7 @@ const config: Config = {
       },
     },
   },
-  // plugins: [globalCSS],
-  plugins: [],
+  // eslint-disable-next-line global-require
+  plugins: [require("./app/globals.css")],
 };
 export default config;
