ef10287941a19db600c51f6ff755352369abcf5c
Aihara <aihara-sama@mail.ru>
2024-08-05 15:08:41 +0300

    feat(navigation-tab): rename

--- components/modals/create-user-modal/tabs/avatar-tab/index.tsx
+++ components/modals/create-user-modal/tabs/avatar-tab/index.tsx
@@ -1,10 +1,11 @@
 import AvatarUpload from "@/components/avatar-upload";
+
 import type { FunctionComponent } from "react";
 
 interface IProps {}
 
 const AvatarTab: FunctionComponent<IProps> = () => {
-  return <AvatarUpload onChange={(_) => {}} />;
+  return <AvatarUpload onChange={() => {}} />;
 };
 
 export default AvatarTab;
