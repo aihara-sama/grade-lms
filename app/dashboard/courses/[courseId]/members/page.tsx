6ba210bb09ed3a5f060ac47c6ea2ccaa1c16e2ae
Aihara <aihara-sama@mail.ru>
2024-08-07 12:24:16 +0300

    feat(refactor): course members

--- app/dashboard/courses/[courseId]/members/page.tsx
+++ app/dashboard/courses/[courseId]/members/page.tsx
@@ -1,6 +1,7 @@
 import CourseHeader from "@/components/course/course-header";
 import Members from "@/components/course/members";
 import { supabaseClient } from "@/utils/supabase/client";
+import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 import { type FunctionComponent } from "react";
@@ -17,12 +18,16 @@ const Page: FunctionComponent<IProps> = async ({ params }) => {
     .eq("id", params.courseId)
     .single();
 
+  const {
+    data: { user },
+  } = await createClient().auth.getUser();
+
   if (error) return redirect("/dashboard/courses");
 
   return (
     <div>
       <CourseHeader course={course} />
-      <Members courseId={params.courseId} />;
+      <Members user={user} courseId={params.courseId} />
     </div>
   );
 };
--- components/course/members/index.tsx
+++ components/course/members/index.tsx
@@ -1,23 +1,32 @@
 "use client";
 
+import CardTitle from "@/components/card-title";
 import CardsContainer from "@/components/cards-container";
-import IconTitle from "@/components/icon-title";
 import AvatarIcon from "@/components/icons/avatar-icon";
+import DeleteIcon from "@/components/icons/delete-icon";
 import SearchIcon from "@/components/icons/search-icon";
 import Input from "@/components/input";
+import Modal from "@/components/modal";
 import Table from "@/components/table";
 import Total from "@/components/total";
 import EnrollUsers from "@/components/users/enroll-users";
 import { ROLES } from "@/interfaces/user.interface";
-import type { Database } from "@/types/supabase.type";
 import { supabaseClient } from "@/utils/supabase/client";
 import { useEffect, useState, type FunctionComponent } from "react";
 import toast from "react-hot-toast";
 
+import IconTitle from "@/components/icon-title";
+import type { Database } from "@/types/supabase.type";
+import type { User as IUser } from "@supabase/supabase-js";
+
 interface IProps {
   courseId: string;
+  user: IUser;
 }
-const Members: FunctionComponent<IProps> = ({ courseId }) => {
+const Members: FunctionComponent<IProps> = ({ courseId, user }) => {
+  const [isDeleteBulkMembersModalOpen, setIsDeleteBulkMembersModalOpen] =
+    useState(false);
+  const [membersIds, setMembersIds] = useState<string[]>([]);
   const [members, setMembers] = useState<
     Database["public"]["Tables"]["users"]["Row"][]
   >([]);
@@ -45,6 +54,20 @@ const Members: FunctionComponent<IProps> = ({ courseId }) => {
     }
   };
 
+  const handleBulkDispelMembers = async () => {
+    const { error } = await supabaseClient
+      .from("user_courses")
+      .delete()
+      .in("user_id", membersIds);
+
+    if (error) toast.error("Something went wrong");
+
+    setMembersIds([]);
+    setIsDeleteBulkMembersModalOpen(false);
+
+    getMembers();
+  };
+
   useEffect(() => {
     getMembers();
   }, []);
@@ -60,18 +83,48 @@ const Members: FunctionComponent<IProps> = ({ courseId }) => {
         />
         <EnrollUsers onDone={getMembers} courseId={courseId} />
       </CardsContainer>
-      <Input Icon={<SearchIcon />} placeholder="Search" />
+      {!membersIds.length ? (
+        <Input
+          Icon={<SearchIcon size="xs" />}
+          placeholder="Search"
+          className="w-auto"
+        />
+      ) : (
+        <div className="mb-3">
+          <button
+            onClick={() => setIsDeleteBulkMembersModalOpen(true)}
+            className="outline-button flex font-semibold gap-2 items-center"
+          >
+            Dispel <DeleteIcon />
+          </button>
+        </div>
+      )}
       <Table
         data={members.map(({ name, role, id }) => ({
-          Name: (
-            <IconTitle
-              Icon={<AvatarIcon size="lg" />}
-              key={id}
-              title={name}
-              subtitle={role}
-              href={`/users/${id}`}
-            />
-          ),
+          Name:
+            user.id === id ? (
+              <IconTitle
+                Icon={<AvatarIcon size="md" />}
+                key={id}
+                title={name}
+                subtitle={role}
+                href={`/users/${id}`}
+              />
+            ) : (
+              <CardTitle
+                href={`/users/${id}`}
+                checked={membersIds.includes(id)}
+                Icon={<AvatarIcon size="md" />}
+                title={name}
+                subtitle={role}
+                onClick={() => {}}
+                onToggle={(checked) =>
+                  checked
+                    ? setMembersIds((prev) => [...prev, id])
+                    : setMembersIds((prev) => prev.filter((_id) => _id !== id))
+                }
+              />
+            ),
           Action: role === ROLES.STUDENT && (
             <button
               className="outline-button"
@@ -83,6 +136,33 @@ const Members: FunctionComponent<IProps> = ({ courseId }) => {
           ),
         }))}
       />
+      {isDeleteBulkMembersModalOpen && (
+        <Modal
+          close={() => setIsDeleteBulkMembersModalOpen(false)}
+          title="Dispel Members"
+          content={
+            <>
+              <p className="mb-4">
+                Are you sure you want to dispel selected members?
+              </p>
+              <div className="group-buttons">
+                <button
+                  className="outline-button w-full"
+                  onClick={() => setIsDeleteBulkMembersModalOpen(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="primary-button"
+                  onClick={handleBulkDispelMembers}
+                >
+                  Dispel
+                </button>
+              </div>
+            </>
+          }
+        />
+      )}
     </>
   );
 };
