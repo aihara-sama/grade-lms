734548ea7f389734bd095b53082f42479eaa57d8
Aihara <aihara-sama@mail.ru>
2024-08-29 21:06:20 +0300

    feat(api): hello world

--- app/api/hello-world/route.ts
+++ app/api/hello-world/route.ts
@@ -2,6 +2,26 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   console.log("Hello World");
+
+  fetch("https://api.emailjs.com/api/v1.0/email/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      service_id: "service_w3mqma7",
+      template_id: "template_bx89lrw",
+      user_id: "RLJnaTuUBzdrR4Wub",
+      accessToken: "OhZfj4KmxFlowUDZjfHA4",
+      template_params: {
+        to_email: "aihara-sama@mail.ru",
+      },
+    }),
+  })
+    .then((r) => r.text())
+    .then((message) => {
+      if (message !== "ok") console.error(message);
+    });
   return NextResponse.json({
     status: "ok",
   });
