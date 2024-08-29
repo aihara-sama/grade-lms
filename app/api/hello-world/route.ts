5f51a67d558c09c97a66b5ba17e1bc9a44257101
Aihara <aihara-sama@mail.ru>
2024-08-29 19:40:35 +0300

    feat(send-emails): set up edge function

---
+++ app/api/hello-world/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  console.log("Hello World");
+
+  return NextResponse.json({
+    status: "ok",
+  });
+}
--- supabase/functions/send-email/index.ts
+++ supabase/functions/send-email/index.ts
@@ -5,26 +5,33 @@ Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
   }
-
+  const body = await req.json();
   try {
-    const res = await fetch("https://api.resend.com/emails", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer re_M45Tzn9t_3U1p7qpA11qRbXqAJwrkcfjs`,
-      },
-      body: JSON.stringify({
-        from: "onboarding@resend.dev",
-        to: JSON.parse(req.body).email,
-        subject: "hello world",
-        html: "<strong>it works!</strong>",
-      }),
-    });
+    for (const user of body.users) {
+      fetch("https://api.emailjs.com/api/v1.0/email/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          service_id: Deno.env.get("EMAILJS_SERVICE_ID"),
+          template_id: Deno.env.get("EMAILJS_TEMPLATE_ID"),
+          user_id: Deno.env.get("EMAILJS_USER_ID"),
+          accessToken: Deno.env.get("EMAILJS_PRIVATE_KEY"),
+          template_params: {
+            to_email: user.email,
+          },
+        }),
+      })
+        .then((r) => r.text())
+        .then((message) => {
+          if (message !== "ok") console.error(message);
+        });
+    }
 
     // const { name } = await req.json();
-    const data = await res.json();
 
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify({}), {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
       status: 200,
     });
