9f5c2db792bf3a8e2790b2a9da3f8e38e8462233
Aihara <aihara-sama@mail.ru>
2024-08-30 15:46:27 +0300

    feat(check-events): respond with users4

--- app/api/check-events/route.ts
+++ app/api/check-events/route.ts
@@ -2,18 +2,17 @@ import { db } from "@/utils/supabase/client";
 import * as admin from "firebase-admin";
 import { NextResponse } from "next/server";
 
-const app = admin.initializeApp(
-  {
-    credential: admin.credential.cert({
-      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    }),
-  },
-  process.env.FIREBASE_APP_NAME
-);
-
 export async function GET() {
+  const app = admin.initializeApp(
+    {
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      }),
+    },
+    process.env.FIREBASE_APP_NAME
+  );
   const { data: users, error: usersError } = await db.rpc(
     "get_upcoming_lessons_users"
   );
