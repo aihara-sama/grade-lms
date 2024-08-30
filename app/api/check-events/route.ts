9967c94394753d68a2295010335e5045028de772
Aihara <aihara-sama@mail.ru>
2024-08-30 16:41:34 +0300

    feat(check-events): respond with users4

--- app/api/check-events/route.ts
+++ app/api/check-events/route.ts
@@ -22,43 +22,14 @@ export async function POST() {
   if (usersError) throw new Error("Failed to get lessons' users");
 
   for (const user of users) {
-    fetch(process.env.EMAILJS_SEND_ENDPOINT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        service_id: process.env.EMAILJS_SERVICE_ID,
-        template_id: process.env.EMAILJS_TEMPLATE_ID,
-        user_id: process.env.EMAILJS_USER_ID,
-        accessToken: process.env.EMAILJS_PRIVATE_KEY,
-        template_params: {
-          to_email: user.email,
+    admin
+      .messaging(app)
+      .send({
+        token: user.fcm_token,
+        notification: {
+          title: "Your lessons starts soon",
+          body: "Your lesson starts soon",
         },
-      }),
-    })
-      .then((r) => r.text())
-      .then((message) => {
-        if (message !== "OK") throw new Error(message);
-      })
-      .then(async () => {
-        const { error } = await db.from("sent_notifications").insert({
-          user_id: user.id,
-          lesson_id: user.lesson_id,
-        });
-        if (error) throw new Error(error.message);
-      })
-      .then(() => {
-        admin
-          .messaging(app)
-          .send({
-            token: user.fcm_token,
-            notification: {
-              title: "Your lessons starts soon",
-              body: "Your lesson starts soon",
-            },
-          })
-          .catch(console.error);
       })
       .catch(console.error);
   }
