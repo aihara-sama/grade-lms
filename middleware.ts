2ba39c36dd91c163a138906ca55134c907fb195a
Aihara <aihara-sama@mail.ru>
2024-09-05 00:51:05 +0300

    fix: KAN-4 next-intl does not run

--- middleware.ts
+++ middleware.ts
@@ -36,7 +36,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
-  ],
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
