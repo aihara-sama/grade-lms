c77e918876d9e44620e2e15a603d95aea72f2b36
Aihara <aihara-sama@mail.ru>
2024-09-05 16:07:11 +0300

    feat: KAN-10

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,31 +1,9 @@
 "use client";
 
-import clsx from "clsx";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
-
-const Cmp = () => {
-  const [hasRendered, setHasRendered] = useState(false);
-
-  useEffect(() => {
-    setHasRendered(true);
-  }, []);
-  return (
-    <div
-      className={`size-20 bg-red-600 transition-all ${clsx(hasRendered && "translate-y-5")}`}
-    ></div>
-  );
-};
 
 const Page: NextPage = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div>
-      <button onClick={() => setIsOpen((_) => !_)}>Toggle</button>
-      {isOpen && <Cmp />}
-    </div>
-  );
+  return <></>;
 };
 
 export default Page;
