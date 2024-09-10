f0ec2943dc477c054c6cbbfec6e3ec68e451cfc4
Aihara <aihara-sama@mail.ru>
2024-09-10 11:38:45 +0300

    feat: KAN-25 lazy loading everywhere

--- components/common/dashboard/dashboard-schedule/index.tsx
+++ components/common/dashboard/dashboard-schedule/index.tsx
@@ -4,13 +4,19 @@ import CalendarWidget from "@/components/calendar-widget";
 import AssignmentsIcon from "@/components/icons/assignments-icon";
 import LessonsIcon from "@/components/icons/lessons-icon";
 import TimeIcon from "@/components/icons/time-icon";
+import { LESSONS_GET_LIMIT } from "@/constants";
 import { useUser } from "@/hooks/use-user";
 import type { Lesson } from "@/types/lessons.type";
+import { isCloseToBottom } from "@/utils/is-document-close-to-bottom";
 import { db } from "@/utils/supabase/client";
+import { throttleFetch } from "@/utils/throttle-fetch";
 import { toCapitalCase } from "@/utils/to-capital-case";
 import { addDays, format, formatDistanceToNow, startOfDay } from "date-fns";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
-import { useEffect, useState, type FunctionComponent } from "react";
+import type { FunctionComponent, UIEventHandler } from "react";
+import { useState, useCallback, useEffect } from "react";
+
 import toast from "react-hot-toast";
 
 const DashboardSchedule: FunctionComponent = () => {
@@ -19,23 +25,8 @@ const DashboardSchedule: FunctionComponent = () => {
   >([]);
   const [scheduleDate, setScheduleDate] = useState(startOfDay(new Date()));
 
+  const t = useTranslations();
   const { user } = useUser();
-
-  const handleDayChange = (date: Date) => {
-    setScheduleDate(startOfDay(date));
-  };
-
-  const fetchLessonsByStartDate = (date: Date) =>
-    db
-      .from("users")
-      .select("id, courses(id, lessons(*, course:courses(title)))")
-      .eq("id", user.id)
-      .gte("courses.lessons.starts", format(date, "yyyy-MM-dd'T'HH:mm:ss"))
-      .lt(
-        "courses.lessons.starts",
-        format(`${startOfDay(addDays(date, 1))}`, "yyyy-MM-dd'T'HH:mm:ss")
-      )
-      .single();
 
   const getWidgetStyle = (date: Date) => {
     let classes = "rounded-xl py-[2px] px-[8px] font-bold border-2 text-sm";
@@ -48,12 +39,53 @@ const DashboardSchedule: FunctionComponent = () => {
     return classes;
   };
 
+  const fetchLessonsByStartDate = (date: Date) =>
+    db
+      .from("users")
+      .select("id, courses(id, lessons(*, course:courses(title)))")
+      .eq("id", user.id)
+      .range(schedule.length, schedule.length + LESSONS_GET_LIMIT - 1, {
+        foreignTable: "courses.lessons",
+      })
+      .gte("courses.lessons.starts", format(date, "yyyy-MM-dd'T'HH:mm:ss"))
+      .lt(
+        "courses.lessons.starts",
+        format(`${startOfDay(addDays(date, 1))}`, "yyyy-MM-dd'T'HH:mm:ss")
+      )
+      .single();
+
+  const fetchMoreLessons = async () => {
+    try {
+      const { data, error } = await fetchLessonsByStartDate(scheduleDate);
+
+      if (error) throw new Error(t("failed_to_load_lessons"));
+
+      if (data.courses.length) {
+        setSchedule((prev) => [
+          ...prev,
+          ...data.courses.map(({ lessons }) => lessons).flat(),
+        ]);
+      }
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
+
+  const onScrollEnd = useCallback(throttleFetch(fetchMoreLessons), [
+    scheduleDate,
+    schedule.length,
+  ]);
+
+  const onScroll: UIEventHandler<HTMLDivElement> = (e) => {
+    if (isCloseToBottom(e.target as HTMLElement, 100)) onScrollEnd();
+  };
+
   useEffect(() => {
     (async () => {
       const { data, error } = await fetchLessonsByStartDate(scheduleDate);
 
       if (error) {
-        toast.error("Something went wrong");
+        toast.error(t("failed_to_load_lessons"));
       } else {
         setSchedule(data.courses.map(({ lessons }) => lessons).flat());
       }
@@ -62,10 +94,13 @@ const DashboardSchedule: FunctionComponent = () => {
 
   return (
     <div>
-      <CalendarWidget onChange={handleDayChange} />
+      <CalendarWidget onChange={(date) => setScheduleDate(startOfDay(date))} />
       <div className="mt-4">
         <h2 className="font-bold text-lg">My schedule</h2>
-        <div className="flex flex-col gap-4 max-h-[800px] [@media(min-width:768px)]:max-h-[492px] [@media(min-width:897px)]:max-h-[300px] overflow-auto pr-2 ">
+        <div
+          className="flex flex-col gap-4 max-h-[800px] [@media(min-width:768px)]:max-h-[492px] [@media(min-width:897px)]:max-h-[300px] overflow-auto pr-2"
+          onScroll={onScroll}
+        >
           {schedule.map((lesson) => (
             <div key={lesson.id}>
               <div className="flex justify-between items-center">
--- utils/is-document-close-to-bottom.ts
+++ utils/is-document-close-to-bottom.ts
@@ -5,11 +5,11 @@ export const isDocCloseToBottom = () => {
   return scrollPosition >= bottomPosition;
 };
 
-export const isCloseToBottom = (element: HTMLElement) => {
+export const isCloseToBottom = (element: HTMLElement, range = 300) => {
   const scrollPosition = element.scrollTop; // Current scroll position
   const { scrollHeight } = element; // Total height of the scrollable area
   const visibleHeight = element.clientHeight; // Height of the visible part of the element
 
   // Check if the user is within 100px of the bottom
-  return scrollHeight - scrollPosition - visibleHeight <= 300;
+  return scrollHeight - scrollPosition - visibleHeight <= range;
 };
