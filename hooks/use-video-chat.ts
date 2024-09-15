c82b393e0f9629c015076df82c3550951cb6c2f5
Aihara <aihara-sama@mail.ru>
2024-09-15 13:45:57 +0300

    feat: KAN-36 better design

--- hooks/use-video-chat.ts
+++ hooks/use-video-chat.ts
@@ -16,8 +16,6 @@ import { useEffect, useRef, useState } from "react";
 
 export const useVideoChat = () => {
   const [cameras, setCameras] = useState<ICamera[]>([]);
-  console.log({ cameras });
-
   const { lessonId } = useParams();
   // Hooks
   const { user } = useUser();
@@ -45,26 +43,9 @@ export const useVideoChat = () => {
     channel.unsubscribe();
   };
   const addCamera = (stream: MediaStream, _user: User) => {
-    setCameras((currentCameras) => {
-      const existingCameraIndex = currentCameras.findIndex(
-        (camera) => camera.stream.id === stream.id
-      );
-
-      if (existingCameraIndex !== -1) {
-        // Replace the existing camera for the user
-        const updatedCameras = [...currentCameras];
-        updatedCameras[existingCameraIndex] = {
-          stream,
-          isCameraEnabled: true,
-          isMicEnabled: true,
-          user: _user,
-        };
-        return updatedCameras;
-      }
-
-      // Add new camera if no camera for the user exists
+    setCameras((_) => {
       return [
-        ...currentCameras,
+        ..._,
         {
           stream,
           isCameraEnabled: true,
@@ -133,14 +114,10 @@ export const useVideoChat = () => {
   const onPresenceJoin = (
     payload: RealtimePresenceJoinPayload<{ user: User }>
   ) => {
-    console.log("onPresenceJoin");
-
     if (payload.key === user.id) {
       Object.keys(channel.presenceState())
         .filter((id) => id !== user.id)
         .forEach((id) => {
-          console.log("Calling...");
-
           const outgoingCall = peerRef.current.call(
             id,
             localStreamRef.current,
@@ -152,16 +129,11 @@ export const useVideoChat = () => {
           );
 
           outgoingCall.on("stream", (remoteStream) => {
-            console.log("outgoingCall - on:stream");
-
             addCamera(
               remoteStream,
               channel.presenceState<{ user: User }>()[id][0].user
             );
-          });
-
-          outgoingCall.on("error", (error) => {
-            alert(error);
+            outgoingCall.off("stream");
           });
         });
     }
@@ -184,13 +156,11 @@ export const useVideoChat = () => {
   };
 
   const onPeerOpen = () => {
-    console.log("onPeerOpen");
+    alert("on peer open");
 
     navigator.mediaDevices
       .getUserMedia({ audio: true, video: true })
       .then((stream) => {
-        console.log("git user media");
-
         channel
           .on("presence", { event: "join" }, onPresenceJoin)
           .on("presence", { event: "leave" }, onPresenceLeave)
@@ -204,24 +174,23 @@ export const useVideoChat = () => {
       });
   };
   const onPeerCall = (incomingCall: MediaConnection) => {
-    console.log("onPeerCall");
-
-    incomingCall.answer(localStreamRef.current);
-    incomingCall.on("stream", (remoteStream) => {
-      console.log("onPeerCall - on:stream");
-
-      addCamera(remoteStream, incomingCall.metadata.user);
-    });
-    incomingCall.on("error", (error) => {
-      alert(error);
-    });
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      .then((stream) => {
+        incomingCall.answer(stream);
+        incomingCall.on("stream", (remoteStream) => {
+          addCamera(remoteStream, incomingCall.metadata.user);
+          incomingCall.off("stream");
+        });
+      })
+      .catch((error) => {
+        console.error("Error accessing media devices: ", error);
+      });
   };
 
   const startSession = () => {
     // Handle SSR for navigator
     import("peerjs").then(({ default: Peer }) => {
-      console.log("Peer imported");
-
       peerRef.current = new Peer(user.id);
 
       peerRef.current.on("open", onPeerOpen);
