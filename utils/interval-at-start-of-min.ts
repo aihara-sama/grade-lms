70eccf001b12fea4fa4ad5ea3cef85b888621792
Aihara <aihara-sama@mail.ru>
2024-09-16 20:33:44 +0300

    feat: KAN-36 better design

--- utils/interval-at-start-of-min.ts
+++ utils/interval-at-start-of-min.ts
@@ -4,28 +4,41 @@ type ExecuteEveryMinute = (fn: (stop: () => void) => void) => () => void;
 
 export const execAtStartOfMin: ExecuteEveryMinute = (fn) => {
   let shouldExec = true;
+  let intervalId: NodeJS.Timeout | null = null;
 
   const stop = () => {
     shouldExec = false;
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
   };
 
-  if (shouldExec) {
-    // Execute the function immediately
-    fn(stop);
-
-    // Calculate the time until the start of the next minute
-    const now = new Date();
-    const nextMinuteStart = startOfMinute(addMinutes(now, 1));
-    const timeUntilNextMinute = differenceInMilliseconds(nextMinuteStart, now);
-
-    // Set a timeout to run the function at the start of the next minute
-    setTimeout(() => {
+  const executeFunction = () => {
+    if (shouldExec) {
       fn(stop);
+    }
+  };
 
-      // After the first timed execution, continue running the function every minute
-      setInterval(fn, 60 * 1000);
-    }, timeUntilNextMinute);
-  }
+  // Execute the function immediately
+  executeFunction();
 
-  return stop;
+  // Calculate the time until the start of the next minute
+  const now = new Date();
+  const nextMinuteStart = startOfMinute(addMinutes(now, 1));
+  const timeUntilNextMinute = differenceInMilliseconds(nextMinuteStart, now);
+
+  // Set a timeout to run the function at the start of the next minute
+  const timeoutId = setTimeout(() => {
+    executeFunction();
+
+    // After the first timed execution, continue running the function every minute
+    intervalId = setInterval(() => {
+      executeFunction();
+    }, 60 * 1000);
+  }, timeUntilNextMinute);
+
+  return () => {
+    stop();
+    clearTimeout(timeoutId);
+  };
 };
