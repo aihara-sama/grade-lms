19b887fb978b9cd9b0ece8e54585d45d2257f83d
Aihara <aihara-sama@mail.ru>
2024-10-19 21:18:41 +0300

    feat: KAN-42

--- app/api/paypal/webhook/route.ts
+++ app/api/paypal/webhook/route.ts
@@ -3,7 +3,7 @@ import crc32 from "buffer-crc32";
 import crypto from "crypto";
 import fetch from "node-fetch";
 
-const { SUPABASE_BUCKET = "paypal-certs", WEBHOOK_ID = "6HN15449TD174563T" } =
+const { SUPABASE_BUCKET = "paypal-certs", WEBHOOK_ID = "7PE795753N136213L" } =
   process.env;
 
 async function downloadAndCache(url: string, cacheKey?: string) {
