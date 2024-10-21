9f156d9862c03368a2d4644a9904809108ca10a5
Aihara <aihara-sama@mail.ru>
2024-10-21 17:13:41 +0300

    feat: KAN-42

--- app/api/paypal/webhook/route.ts
+++ app/api/paypal/webhook/route.ts
@@ -1,7 +1,6 @@
 import { adminDB } from "@/lib/supabase/db/admin-db";
 import crc32 from "buffer-crc32";
 import crypto from "crypto";
-import { addMonths } from "date-fns";
 import fetch from "node-fetch";
 
 const { SUPABASE_BUCKET = "paypal-certs", WEBHOOK_ID = "6HN15449TD174563T" } =
@@ -88,25 +87,6 @@ export async function POST(req: Request) {
       if (error) {
         console.log("error saving sub", { error });
 
-        return Response.json({}, { status: 500 });
-      }
-    }
-
-    if (body.event_type === "BILLING.SUBSCRIPTION.CANCELLED") {
-      console.log("billing_info", body.resource.billing_info);
-      console.log("billing_info", body.resource.billing_info.last_payment);
-
-      const { error } = await adminDB
-        .from("subscriptions")
-        .update({
-          end_date: addMonths(
-            body.resource.billing_info.last_payment.time as string,
-            1
-          ).toISOString(),
-        })
-        .eq("paypal_subscription_id", body.resource.id);
-
-      if (error) {
         return Response.json({}, { status: 500 });
       }
     }
